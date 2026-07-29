6531603f6c25de7bf751aba79130348a1547970b
chrisbautista09 <chrisbautista.cb@gmail.com>
2026-07-29 16:50:00 +0200

    feat(auth): implement user registration and login

--- backend/src/Entity/User.php
+++ backend/src/Entity/User.php
@@ -6,6 +6,7 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
@@ -13,6 +14,11 @@ use Symfony\Component\Security\Core\User\UserInterface;
 #[ORM\UniqueConstraint(
     name: 'UNIQ_IDENTIFIER_EMAIL',
     fields: ['email']
+)]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+#[UniqueEntity(
+    fields: ['pseudo'],
+    message: 'Ce pseudo est déjà utilisé.'
 )]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
